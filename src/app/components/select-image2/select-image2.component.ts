c334edb4827b58edeed75634a8826f6ecd79ae42
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-04-26 01:43:24 -0400

    Using ngFor working

--- src/app/components/select-image2/select-image2.component.ts
+++ src/app/components/select-image2/select-image2.component.ts
@@ -7,7 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectImage2Component implements OnInit {
 
-  constructor() { }
+  public stampsURLList: Array<{ id: number, url: string }>;
+
+  constructor() {
+    this.stampsURLList = new Array<{ id: number, url: string }>();
+
+    this.stampsURLList.push({
+      id: 1,
+      url: 'https://www.usps.com/ecp/asset/images/749704-L0.jpg'
+    });
+    this.stampsURLList.push({
+      id: 2,
+      url: 'https://previews.123rf.com/images/nalinn/nalinn1508/nalinn150800018/44272522-red-grunge-approved-rubber-stamp-isolated-on-white-background.jpg'
+    });
+    this.stampsURLList.push({
+      id: 3,
+      url: 'https://image.shutterstock.com/image-vector/red-grunge-approved-rubber-stamp-260nw-287233613.jpg'
+    });
+
+    this.stampsURLList.push({
+      id: 4,
+      url: 'https://previews.123rf.com/images/nalinn/nalinn1508/nalinn150800018/44272522-red-grunge-approved-rubber-stamp-isolated-on-white-background.jpg'
+    });
+    this.stampsURLList.push({
+      id: 5,
+      url: 'https://www.usps.com/ecp/asset/images/749704-L0.jpg'
+    });
+    this.stampsURLList.push({
+      id: 6,
+      url: 'https://image.shutterstock.com/image-vector/red-grunge-approved-rubber-stamp-260nw-287233613.jpg'
+    });
+
+    this.stampsURLList.push({
+      id: 7,
+      url: 'https://image.shutterstock.com/image-vector/red-grunge-approved-rubber-stamp-260nw-287233613.jpg'
+    });
+    this.stampsURLList.push({
+      id: 8,
+      url: 'https://www.usps.com/ecp/asset/images/749704-L0.jpg'
+    });
+    this.stampsURLList.push({
+      id: 9,
+      url: 'https://previews.123rf.com/images/nalinn/nalinn1508/nalinn150800018/44272522-red-grunge-approved-rubber-stamp-isolated-on-white-background.jpg'
+    });
+  }
 
   ngOnInit(): void {
   }
